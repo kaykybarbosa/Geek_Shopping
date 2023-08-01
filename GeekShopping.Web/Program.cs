e7a04ca309b6ca3369766a861e1827607cc237a5
kayky <kaykyb678@gmail.com>
2023-08-01 16:27:17 -0300

    Add configuration the Service

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,7 +1,14 @@
+using GeekShopping.Web.Interfaces;
+using GeekShopping.Web.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient<IProductService, ProductService>(
+    c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductApi"])
+);
 
 var app = builder.Build();
 
