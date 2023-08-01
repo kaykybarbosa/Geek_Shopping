cc38f5decafc9d7683657a40d6a8499d91c62217
kayky <kaykyb678@gmail.com>
2023-08-01 16:57:35 -0300

    Add Images, Controller and Configurations

---
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using GeekShopping.Web.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            var products = await _productService.FindAllProducts();
+
+             return View(products);
+        }
+    }
+}
