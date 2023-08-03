1cac15ee2bb03855e94bcbf9cbc15d5d9d7d9f25
kayky <kaykyb678@gmail.com>
2023-08-03 16:45:59 -0300

    Drop BaseResponse the Product

--- GeekShopping/Dtos/BaseResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GeekShopping.Dtos
-{
-    public class BaseResponse
-    {
-        public string? Message { get; set; }
-        public bool IsSuccess { get; set; }
-        public int StatusCode { get; set; }
-    }
-}
