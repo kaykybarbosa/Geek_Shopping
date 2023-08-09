e19382782f4e93e4688d1d396eee0cbae00aa81e
kayky <kaykyb678@gmail.com>
2023-08-09 15:33:38 -0300

    Setting custom claims

---
+++ GeekShopping.IdentityServer/Services/ProfileService.cs
@@ -0,0 +1,68 @@
+﻿using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using GeekShopping.IdentityServer.MyModels;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace GeekShopping.IdentityServer.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUserClaimsPrincipalFactory<ApplicationUser> _userClaimsPrincipal;
+
+        public ProfileService(UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager, 
+            IUserClaimsPrincipalFactory<ApplicationUser> userClaimsPrincipal)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _userClaimsPrincipal = userClaimsPrincipal;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            string id = context.Subject.GetSubjectId();
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+
+            ClaimsPrincipal userClaims = await _userClaimsPrincipal.CreateAsync(user);
+
+            List<Claim> claims = userClaims.Claims.ToList();
+
+            claims.Add(new Claim(JwtClaimTypes.FamilyName, user.FirtsName));
+            claims.Add(new Claim(JwtClaimTypes.GivenName, user.LastName));
+
+            if (_userManager.SupportsUserRole)
+            {
+                IList<string> roles = await _userManager.GetRolesAsync(user);
+
+                foreach(string role in roles)
+                {
+                    claims.Add(new Claim(JwtClaimTypes.Role, role));
+
+                    if (_roleManager.SupportsRoleClaims)
+                    {
+                        IdentityRole identityRole = await _roleManager.FindByNameAsync(role);
+
+                        if (identityRole != null)
+                        {
+                            claims.AddRange(await _roleManager.GetClaimsAsync(identityRole));
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            string id = context.Subject.GetSubjectId();
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+
+            context.IsActive = user != null;
+        }
+    }
+}
