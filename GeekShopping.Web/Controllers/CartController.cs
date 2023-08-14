a5a5cc15d510de132596fdda83641ada33a17b11
kayky <kaykyb678@gmail.com>
2023-08-14 14:46:59 -0300

    Implementation of the method responsible for locating the User Cart.

---
+++ GeekShopping.Web/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using GeekShopping.Web.Interfaces;
+using GeekShopping.Web.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Specialized;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(IProductService productService, ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        public async Task<IActionResult> CartIndex()
+        {
+            var response = await FindUserCart();
+
+            return View(response);
+        }
+
+        private async Task<CartViewModel> FindUserCart()
+        {
+            string token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.FindCartByUserId(userId, token);
+
+            if (response?.CartHeader != null)
+            {
+                foreach (var detail in response.CartDetails)
+                {
+                    var price = detail.Product.Price;
+                    var count = detail.Product.Count;
+
+                    response.CartHeader.PurchaseAmount += (price * count);
+                }
+            }
+
+            return response;
+        }
+    }
+}
