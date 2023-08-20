2a7a40d5f346a0b9c390323ceca4089c890d9b95
Kayky Barbosa <kaykybarbosa69@hotmail.com>
2023-08-20 18:03:43 -0300

    Implementation of the service Consumer Message

--- GeekShopping.OrderApi/Dtos/CheckoutHeaderResponse.cs
+++ GeekShopping.OrderApi/Dtos/CheckoutHeaderResponse.cs
@@ -13,10 +13,11 @@ namespace GeekShopping.OrderApi.Dtos
         public string LastName { get; set; }
         public DateTime DateTime { get; set; }
         public string Phone { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; }   
         public string CardNumber { get; set; }
         public string CVV { get; set; }
         public string ExpiryMonthYear { get; set; }
+
         public int CartTotalItens { get; set; }
         public IEnumerable<CartDetailResponse> CartDetails { get; set; }
     }
---
+++ GeekShopping.OrderApi/Services/RabbitMQCheckoutConsumer.cs
@@ -0,0 +1,92 @@
+﻿using GeekShopping.OrderApi.Dtos;
+using GeekShopping.OrderApi.Model;
+using GeekShopping.OrderApi.Repository;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace GeekShopping.OrderApi.Services
+{
+    public class RabbitMQCheckoutConsumer : BackgroundService
+    {
+        private readonly OrderRepository _orderRepository;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMQCheckoutConsumer(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "checkoutqueue", false, false, false, null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (channel, evt) =>
+            {
+                var context = Encoding.UTF8.GetString(evt.Body.ToArray());
+                CheckoutHeaderResponse response = JsonSerializer.Deserialize<CheckoutHeaderResponse>(context);
+ 
+                ProcessOrder(response).GetAwaiter().GetResult();
+
+                _channel.BasicAck(evt.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume("checkoutqueue", false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task ProcessOrder(CheckoutHeaderResponse response)
+        {
+            OrderHeader orderHeader = new()
+            {
+                UserId = response.UserId,
+                CouponCode = response.CouponCode,
+                PurchaseAmount = response.PurchaseAmount,
+                DiscountAmount = response.DiscountAmount,
+                FirstName = response.FirstName,
+                LastName = response.LastName,
+                DateTime = response.DateTime,
+                OrderTime = DateTime.Now,
+                Phone = response.Phone,
+                Email = response.Email,
+                CardNumber = response.CardNumber,
+                CVV = response.CVV,
+                ExpiryMonthYear = response.ExpiryMonthYear,
+                PaymentStatus = false,
+                OrderDetails = new List<OrderDetail>()
+            };
+
+            foreach(var details in response.CartDetails)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    ProductId = details.ProductId,
+                    ProductName = details.Product.Name,
+                    Price = details.Product.Price,
+                    Count = details.Count
+                };
+
+                orderHeader.CartTotalItens += details.Count;
+                orderHeader.OrderDetails.Add(orderDetail);
+            }
+
+            if(orderHeader != null)
+            {
+                await _orderRepository.AddOrderHeader(orderHeader);
+            }
+        }
+    }
+ } 
