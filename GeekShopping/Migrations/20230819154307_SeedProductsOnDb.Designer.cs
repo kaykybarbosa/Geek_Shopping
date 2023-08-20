58593da1d1f0d0699771141eb866ea24d6ad06d4
Kayky Barbosa <kaykybarbosa69@hotmail.com>
2023-08-20 17:44:54 -0300

    Adding data to the DataBase

--- GeekShopping/Migrations/20230819154307_SeedProductsOnDb.Designer.cs
+++ GeekShopping/Migrations/20230819154307_SeedProductsOnDb.Designer.cs
@@ -3,6 +3,7 @@ using GeekShopping.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeekShopping.ProductApi.Migrations
 {
     [DbContext(typeof(MySqlContext))]
-    partial class MySqlContextModelSnapshot : ModelSnapshot
+    [Migration("20230819154307_SeedProductsOnDb")]
+    partial class SeedProductsOnDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
