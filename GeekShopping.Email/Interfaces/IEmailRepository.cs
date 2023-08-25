c5e68a6783be757ea9bee33395d7d5efc3f3b08a
kayky <kaykyb678@gmail.com>
2023-08-25 10:27:36 -0300

    Implementation of the email sending service

--- GeekShopping.Email/Interfaces/IEmailRepository.cs
+++ GeekShopping.Email/Interfaces/IEmailRepository.cs
@@ -1,9 +1,12 @@
 ﻿using GeekShopping.Email.Dto;
+using GeekShopping.Email.Model;
+using System.Net.Mail;
 
 namespace GeekShopping.Email.Interfaces
 {
     public interface IEmailRepository
     {
         Task LogEmail(PaymentResultUpdateMessage message);
+        void SendEmail(EmailLog email);
     }
 }
--- GeekShopping.Email/Repositories/EmailRepository.cs
+++ GeekShopping.Email/Repositories/EmailRepository.cs
@@ -3,15 +3,19 @@ using GeekShopping.Email.Interfaces;
 using GeekShopping.Email.Model;
 using GeekShopping.Email.Model.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Net.Mail;
 
 namespace GeekShopping.Email.Repositories
 {
     public class EmailRepository : IEmailRepository
     {
         private readonly DbContextOptions<MySqlContextEmail> _context;
+        private MailMessage _email;
         public EmailRepository(DbContextOptions<MySqlContextEmail> context)
         {
             _context = context;
+            _email = new MailMessage();
         }
 
         public async Task LogEmail(PaymentResultUpdateMessage message)
@@ -27,6 +31,35 @@ namespace GeekShopping.Email.Repositories
             _db.Emails.Add(email);
 
             await _db.SaveChangesAsync();
+
+            SendEmail(email);
+        }
+
+        public void SendEmail(EmailLog email)
+        {
+            _email.From = new MailAddress("GeekShopping@gmail.com");
+
+            _email.Subject = "GEEK SHOPPING, YOUR PURCHASE!";
+            _email.Body = "Congratulations, we have received your request and are preparing for shipment. Thank you very much for purchasing our products.";
+
+            _email.To.Add(email.Email);
+            _email.Priority = MailPriority.Normal;
+
+            var smtp = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential("GeekShopping@gmail.com", "passwordGeekShopping"),
+                UseDefaultCredentials = false,
+            };
+
+            try
+            {
+                smtp.Send(_email);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
