9d504edbedca0e6c8ac91026fbc694ba41e14eee
Kayky Barbosa <kaykyb629@gmail.com>
2023-09-14 11:20:59 -0300

    Alter: making some adjustments

---
+++ GeekShopping.Email/Migrations/20230911180008_AddEmailLogInDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.Email.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailLogInDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
