84e15f0d8c6f68bd0a383c551e26876e0fb5ae76
Kayky Barbosa <kaykyb629@gmail.com>
2023-10-02 15:27:41 -0300

    Alter: adding new migrations

---
+++ GeekShopping/Model/Migrations/20230801132855_FirstMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PRODUCTS",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NANE = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    PRICE = table.Column<decimal>(type: "decimal(18,0)", nullable: false),
+                    DESCRIPTION = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CATEGORY_NAME = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IMAGE_URL = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PRODUCTS", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PRODUCTS");
+        }
+    }
+}
